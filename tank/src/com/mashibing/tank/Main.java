312471b8cb88c7ae061f09ddf9fee6d418b5c696
bjmashibing <alanpppbox@gmail.com>
2019-04-30 21:07:59 +0800

    new出五个敌人坦克

--- tank/src/com/mashibing/tank/Main.java
+++ tank/src/com/mashibing/tank/Main.java
@@ -5,6 +5,11 @@ public class Main {
 	public static void main(String[] args) throws InterruptedException {
 		TankFrame tf = new TankFrame();
 		
+		//初始化敌方坦克
+		for(int i=0; i<5; i++) {
+			tf.tanks.add(new Tank(50 + i*80, 200, Dir.DOWN, tf));
+		}
+		
 		while(true) {
 			Thread.sleep(50);
 			tf.repaint();
--- tank/src/com/mashibing/tank/TankFrame.java
+++ tank/src/com/mashibing/tank/TankFrame.java
@@ -14,8 +14,10 @@ import java.util.List;
 
 public class TankFrame extends Frame {
 
-	Tank myTank = new Tank(200, 200, Dir.DOWN, this);
+	Tank myTank = new Tank(200, 400, Dir.DOWN, this);
 	List<Bullet> bullets = new ArrayList<>();
+	List<Tank> tanks = new ArrayList<>();
+	
 	static final int GAME_WIDTH = 800, GAME_HEIGHT = 600;
 
 	public TankFrame() {
@@ -63,7 +65,12 @@ public class TankFrame extends Frame {
 		for (int i = 0; i < bullets.size(); i++) {
 			bullets.get(i).paint(g);
 		}
+		
+		for (int i = 0; i < tanks.size(); i++) {
+			tanks.get(i).paint(g);
+		}
 
+		
 		// for(Iterator<Bullet> it = bullets.iterator(); it.hasNext();) {
 		// Bullet b = it.next();
 		// if(!b.live) it.remove();
