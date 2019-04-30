3baceaa93ee90e3e53ba8c6e0168982ef11f9d09
bjmashibing <alanpppbox@gmail.com>
2019-04-30 20:44:44 +0800

    加入图片

---
+++ tank/src/com/mashibing/tank/ResourceMgr.java
@@ -0,0 +1,21 @@
+package com.mashibing.tank;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class ResourceMgr {
+	public static BufferedImage tankL, tankU, tankR, tankD; 
+	
+	static {
+		try {
+			tankL = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/tankL.gif"));
+			tankU = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/tankU.gif"));
+			tankR = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/tankR.gif"));
+			tankD = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/tankD.gif"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
